cec1d212b7f244b1eb70ff55e3abbd724e93ff20
Gustavo <gumrqss@gmail.com>
2022-09-03 19:46:30 -0300

    Post participants pronta

--- index.js
+++ index.js
@@ -1,3 +1,88 @@
 import express, {json} from "express";
 import cors from "cors";
+import {MongoClient} from 'mongodb';
+import dotenv from 'dotenv';
+import joi from 'joi';
+import dayjs from 'dayjs';
 
+dotenv.config();
+
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+const mongoClient = new MongoClient(process.env.MONGO_URI)
+
+
+let db;
+
+const participantsSchema = joi.object({ 
+    name: joi.string().required()
+})
+
+mongoClient.connect().then(()=>{
+    db = mongoClient.db(process.env.DB_NAME);
+});
+
+server.post('/participants', async(req,res)=>{
+
+    const user = req.body;
+
+    try{
+        let day = dayjs().locale('pt-br');
+        const invalidName = participantsSchema.validate(user).error;
+
+        if(invalidName){
+        
+            return res.status(422).send('Formato inválido')
+        }
+
+        const userOnline = await db.collection('users').findOne({name:user.name});
+
+            console.log(userOnline);
+
+            if(userOnline !== null){
+                return res.status(409).send('Usuário já existente')
+            }
+        
+        
+        const response = await db.collection('users').insertOne({
+            name: user.name,
+            lastStatus: Date.now()
+        });
+        const LoginMessage = await db.collection('messages').insertOne({
+            from: user.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time:  day.format('HH:MM:ss')
+        })
+        const messages = await db.collection('messages').findOne({from: user.name});
+        console.log(messages, "alo alo")
+        res.status(200).send('Usuário registrado com sucesso');
+
+    } catch (error){
+        res.status(500).send(error.message);
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 5000;
+
+server.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
